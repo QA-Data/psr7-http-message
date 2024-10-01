df1b5c9e2324b7efeb247bbecad7a9d7d31aca35
Igor Počta <hello@igorpocta.cz>
2024-10-01 17:23:42 +0200

    Fix nullable return type

--- src/NullStream.php
+++ src/NullStream.php
@@ -81,7 +81,7 @@ class NullStream implements StreamInterface
 		throw new RuntimeException('Null streams cannot write');
 	}
 
-	public function getMetadata(string|null $key = null): null
+	public function getMetadata(string|null $key = null): mixed
 	{
 		return null;
 	}
